ebf2486342a57aa052f75a0b2bea2ada402c0184
Eryk Ciesielski <eryk-ciesielski@outlook.com>
2025-08-25 10:05:41 +0200

    Create ingredient form

---
+++ FitnessWebApp/Controllers/IngredientController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessWebApp.Controllers
+{
+    public class IngredientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
